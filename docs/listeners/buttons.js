fdbf5e0586bf2bc72a1abaa1643f2f189b825d07
Ruben Lousada <rlousada123456@gmail.com>
2021-08-29 15:43:47 +0100

    removed commented code

--- docs/listeners/buttons.js
+++ docs/listeners/buttons.js
@@ -1,14 +1,14 @@
-//import * as app from "../app.js";
-//import * as generator from "../map/GenerateMap.js";
+import * as app from "../app.js";
+import * as generator from "../map/GenerateMap.js";
 export function activateButton() {
-    var _a, _b;
-    //document?.querySelector("#findPoint")?.addEventListener("click", () => { app.findPoint() });
-    (_a = document === null || document === void 0 ? void 0 : document.querySelector("#randomWalls")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
-        //generator.generateRandomWalls();
-        //app.findPoint();
+    var _a, _b, _c;
+    (_a = document === null || document === void 0 ? void 0 : document.querySelector("#findPoint")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => { app.findPoint(); });
+    (_b = document === null || document === void 0 ? void 0 : document.querySelector("#randomWalls")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", () => {
+        generator.generateRandomWalls();
+        app.findPoint();
     });
-    (_b = document === null || document === void 0 ? void 0 : document.querySelector("#clearWall")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", () => {
-        //generator.clearWalls();
-        //app.findPoint();
+    (_c = document === null || document === void 0 ? void 0 : document.querySelector("#clearWall")) === null || _c === void 0 ? void 0 : _c.addEventListener("click", () => {
+        generator.clearWalls();
+        app.findPoint();
     });
 }
--- src/listeners/Buttons.ts
+++ src/listeners/Buttons.ts
@@ -1,16 +1,16 @@
 
-//import * as app from "../app.js";
-//import * as generator from "../map/GenerateMap.js";
+import * as app from "../app.js";
+import * as generator from "../map/GenerateMap.js";
 
 export function activateButton () {
-    //document?.querySelector("#findPoint")?.addEventListener("click", () => { app.findPoint() });
+    document?.querySelector("#findPoint")?.addEventListener("click", () => { app.findPoint() });
     document?.querySelector("#randomWalls")?.addEventListener("click", () => { 
-        //generator.generateRandomWalls();
-        //app.findPoint();
+        generator.generateRandomWalls();
+        app.findPoint();
     });
 
     document?.querySelector("#clearWall")?.addEventListener("click", () => { 
-        //generator.clearWalls();
-        //app.findPoint();
+        generator.clearWalls();
+        app.findPoint();
     });
 }
