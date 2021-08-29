d0c240398426d420ec3e093041fda5c411aa5764
Ruben Lousada <rlousada123456@gmail.com>
2021-08-29 15:22:35 +0100

    dragndrop listener activted

--- src/listeners/ListenersActivator.ts
+++ src/listeners/ListenersActivator.ts
@@ -1,11 +1,11 @@
 //import * as buttonListener from "../listeners/Buttons.js"
-//import * as dragndropListener from "../listeners/DragNDrop.js"
+import * as dragndropListener from "../listeners/DragNDrop.js"
 import * as optionSelectorListener from "../listeners/OptionSelector.js"
 import * as createWalls from "../listeners/CreateWalls.js"
 
 export function activateListeners() {
     //buttonListener.activateButton();
-    //dragndropListener.activateDragPoints();
+    dragndropListener.activateDragPoints();
     optionSelectorListener.activateOptionSelector();
     createWalls.activateCreateWallsEvent();
 }
