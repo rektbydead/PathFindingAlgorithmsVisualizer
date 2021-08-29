71e9b64ca520c1868f040a394a9e40d35ab6f107
Ruben Lousada <rlousada123456@gmail.com>
2021-08-29 16:30:29 +0100

    remove log

--- docs/listeners/ListenersActivator.js
+++ docs/listeners/ListenersActivator.js
@@ -3,13 +3,8 @@ import * as dragndropListener from "./DragNDrop.js";
 import * as optionSelectorListener from "./OptionSelector.js";
 import * as createWalls from "./CreateWalls.js";
 export function activateListeners() {
-    console.log("1");
     buttonListener.activateButton();
-    console.log("2");
     dragndropListener.activateDragPoints();
-    console.log("3-");
     optionSelectorListener.activateOptionSelector();
-    console.log("4");
     createWalls.activateCreateWallsEvent();
-    console.log("5");
 }
--- src/listeners/ListenersActivator.ts
+++ src/listeners/ListenersActivator.ts
@@ -4,13 +4,8 @@ import * as optionSelectorListener from "./OptionSelector.js"
 import * as createWalls from "./CreateWalls.js"
 
 export function activateListeners() {
-    console.log("1");
     buttonListener.activateButton();
-    console.log("2");
     dragndropListener.activateDragPoints();
-    console.log("3-");
     optionSelectorListener.activateOptionSelector();
-    console.log("4");
     createWalls.activateCreateWallsEvent();
-    console.log("5");
 }
