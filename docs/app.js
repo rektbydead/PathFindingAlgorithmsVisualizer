98987befcbaf8f105148cf2a3f784f362c6ffb5c
Ruben Lousada <rlousada123456@gmail.com>
2021-08-29 14:11:20 +0100

    recompiled

--- docs/app.js
+++ docs/app.js
@@ -5,8 +5,11 @@ import * as DJ from "./algorithm/Dijkstra.js";
 import * as retraceUtils from "./utils/RetracePathUtils.js";
 import * as BFS from "./algorithm/Breadth-first-search.js";
 import * as DFS from "./algorithm/Depth-First-Search.js";
+console.log("0");
 generator.generateEmptyMap();
+console.log("1");
 listener.activateListeners();
+console.log("2");
 function startAlgorithm(table) {
     const select = document === null || document === void 0 ? void 0 : document.getElementById("algorithmSelector");
     const type = select.options[select.selectedIndex].value;
--- docs/utils/ArrayFunctions.js
+++
@@ -1,8 +0,0 @@
-export function removeFromArray(list, node) {
-    return list = list.filter((dot, index, array) => {
-        return !(dot.x == node.x && dot.y == node.y);
-    });
-}
-export function contains(list, node) {
-    return list.some((dotN) => node.x == dotN.x && node.y == dotN.y);
-}
