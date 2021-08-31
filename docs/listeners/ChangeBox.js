95f9d8765615a6b6d4bb1333e49edb4ab52111f3
Ruben Lousada <rlousada123456@gmail.com>
2021-08-31 21:22:29 +0100

    on change

--- docs/listeners/ChangeBox.js
+++ docs/listeners/ChangeBox.js
@@ -1,7 +1,16 @@
 export function changeBox() {
     const element = document.getElementById('algorithmSelector');
     const type = element.options[element.selectedIndex].value;
-    element === null || element === void 0 ? void 0 : element.addEventListener('change', () => {
-        console.log(element.value);
+    element === null || element === void 0 ? void 0 : element.addEventListener('change', (e) => {
+        const value = element.value;
+        console.log(value);
+        let isHeuristic = value == "ASTAR" || value == "DIJKSTRA";
+        const elementToChange = document.getElementById('heuristicSelector');
+        if (isHeuristic) {
+            elementToChange.style.display = "none";
+        }
+        else {
+            elementToChange.style.display = "initial";
+        }
     });
 }
--- src/listeners/ChangeBox.ts
+++ src/listeners/ChangeBox.ts
@@ -1,8 +1,22 @@
+import { getColorType } from "../objects/DOT";
+
 export function changeBox() {
-    const element = document.getElementById('algorithmSelector') as HTMLSelectElement;
+    const element : HTMLSelectElement = document.getElementById('algorithmSelector') as HTMLSelectElement;
     const type : string = element.options[element.selectedIndex].value;
 
-    element?.addEventListener('change', () => {
-        console.log(element.value);
+    element?.addEventListener('change', (e) => {
+        const value : string  = element.value;
+        console.log(value);
+        
+        let isHeuristic : boolean = value == "ASTAR" || value == "DIJKSTRA";
+
+        const elementToChange : HTMLSelectElement = document.getElementById('heuristicSelector') as HTMLSelectElement;
+
+        if (isHeuristic) {
+            elementToChange.style.display = "none"
+        }
+        else { 
+            elementToChange.style.display = "initial"
+        }
     });
 }
