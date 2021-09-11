c3a1d4d7ee1db4377f0d41a3292503a6b0530bc9
Ruben Lousada <rlousada123456@gmail.com>
2021-09-11 17:29:02 +0100

    bug fixed ... wouldnt try vertices

--- docs/utils/AlgorithmUtils.js
+++ docs/utils/AlgorithmUtils.js
@@ -2,24 +2,27 @@ import { TYPE } from "../objects/DOT.js";
 export function getNeighbors(table, dot) {
     let neighbors = [];
     const element = document.getElementById('Diagonal');
-    let diagonal = element.checked;
     let check = 0;
-    let avoid = [1, 3, 7, 9];
+    let vertices = [1, 3, 7, 9];
     for (let x = dot.x - 1; x <= dot.x + 1; x++) {
         for (let y = dot.y - 1; y <= dot.y + 1; y++) {
-            if (x < 0 || y < 0 || x >= table.length || y >= table[0].length)
-                continue;
-            if (avoid.indexOf(++check) != -1 && !diagonal) {
-                continue;
-            }
-            if (x == dot.x && y == dot.y) {
+            const isVertices = vertices.indexOf(++check) != -1;
+            const allowVertices = element.checked;
+            //dot is invalid
+            if (x < 0 || y < 0 || x >= table.length || y >= table[0].length) {
                 continue;
             }
+            //dot is wall
             if (table[x][y].type == TYPE.WALL) {
                 continue;
             }
-            /*if (table[x][y].type != TYPE.START && table[x][y].type != TYPE.END)
-            table[x][y].child.style.backgroundColor = "purple";*/
+            //its the dot itself
+            if (x == dot.x && y == dot.y) {
+                continue;
+            }
+            if (!allowVertices && isVertices) {
+                continue;
+            }
             neighbors.push(table[x][y]);
         }
     }
--- src/utils/AlgorithmUtils.ts
+++ src/utils/AlgorithmUtils.ts
@@ -4,30 +4,34 @@ import { getTypeColor, getColorType, getInverseType, DOT, TYPE } from "../object
 export function getNeighbors(table : DOT[][], dot : DOT) : Array<DOT> {
     let neighbors: Array<DOT> = [];
 
-    const element = document.getElementById('Diagonal') as HTMLInputElement;
-    let diagonal = element.checked;
+    const element : HTMLInputElement = document.getElementById('Diagonal') as HTMLInputElement;
 
     let check: number = 0;
-    let avoid: Array<number> = [1, 3, 7, 9];
+    let vertices: Array<number> = [1, 3, 7, 9];
     for(let x : number = dot.x - 1; x <= dot.x + 1; x++) {
         for(let y : number = dot.y - 1; y <= dot.y + 1; y++) {
-            if (x < 0 || y < 0 || x >= table.length || y >= table[0].length)
-                continue;
+            const isVertices : boolean = vertices.indexOf(++check) != -1;
+            const allowVertices : boolean = element.checked;
 
-            if (avoid.indexOf(++check) != -1 && !diagonal){
-                continue 
+            //dot is invalid
+            if (x < 0 || y < 0 || x >= table.length || y >= table[0].length) {
+                continue;
+            }            
+
+            //dot is wall
+            if (table[x][y].type == TYPE.WALL) {
+                continue;
             }
-            
+
+            //its the dot itself
             if (x == dot.x && y == dot.y) {
                 continue;
             }
 
-            if (table[x][y].type == TYPE.WALL) {
-                continue;
+            if (!allowVertices && isVertices){
+                continue 
             }
             
-            /*if (table[x][y].type != TYPE.START && table[x][y].type != TYPE.END)
-            table[x][y].child.style.backgroundColor = "purple";*/
             neighbors.push(table[x][y]);
         }
     }
